9234504b3abf971a5b483e75a59914e1d6cd8c0a
Thalys Farias <thalysfarias11@gmail.com>
2024-04-11 11:35:33 -0300

    feat: adjust prettier and eslint config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 100,
-    tabWidth: 2,
-    useTabs: false,
-    bracketSameLine: false,
-    bracketSpacing: true,
-    arrowParens: 'always',
-    endOfLine: 'lf',
-  };
-  
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
